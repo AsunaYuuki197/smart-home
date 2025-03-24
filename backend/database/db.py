1c274e59efb6c39eb85bde0639356b8b54009b31
AsunaYuuki197 <tamhau197@gmail.com>
2025-03-24 14:29:09 +0700

    Add database connection

---
+++ backend/database/db.py
@@ -0,0 +1,18 @@
+import motor.motor_asyncio
+import os 
+from dotenv import load_dotenv
+load_dotenv()
+
+DB_NAME = os.getenv("DB_NAME")
+DB_PWD = os.getenv("DB_PWD")
+
+MONGO_URL = f"mongodb+srv://{DB_NAME}:{DB_PWD}@testmongodb.sovhh.mongodb.net/?retryWrites=true&w=majority&appName=TestMongodb"
+
+try: 
+    client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URL)
+except Exception as e:
+    print(e)
+    
+db = client.TestMongodb
+
+
