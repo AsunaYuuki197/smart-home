d55aaa8d128e3efd29fddd977721bad1d8a76d1a
huybk04 <huy.nguyenquang2004@hcmut.edu.vn>
2025-04-13 08:00:49 +0700

    Add authorization dependency in every routes

--- backend/routes/base.py
+++ backend/routes/base.py
@@ -3,9 +3,12 @@ from fastapi import APIRouter
 from .control import router as control_route
 from .general import router as general_route
 from .autorule import router as autorule
+from auth.auth import secure_router
 
 router = APIRouter()
 # router.include_router(ai_route, prefix="/function-calling")
 router.include_router(control_route, prefix="/device")
 router.include_router(autorule, prefix="/autorule")
 router.include_router(general_route)
+
+secure_router(router)
